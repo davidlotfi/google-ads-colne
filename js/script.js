7d4f4c0d54347add8f35c8f10c80f65d4061c719
davidlotfi <boukernineabdellatif@gmail.com>
2023-01-03 19:28:01 +0100

    add js file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let form = document.getElementsByClassName('form_etap');
+let nextBtn = document.getElementById('next-btn');
+let prevBtn = document.getElementById('prev-btn');
+
+let current_step = 0;
+let stepCount = 3;
