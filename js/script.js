a0772cad1f5c6e44f707bbf33fec32e170d7a9d7
davidlotfi <boukernineabdellatif@gmail.com>
2023-01-04 21:05:29 +0100

    up code

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ prevBtn.classList.add('d-none');
 terminerBtn.classList.add('d-none');
 }
 
+
+const progress = (value) => {
+document.getElementsByClassName('progress-bar')[0].style.width = `${value}%`;
+}
+
 nextBtn.addEventListener('click',() => {
 current_step++;
 let previous_step = current_step - 1;
@@ -28,11 +33,32 @@ if ((current_step > 0) && (current_step <= stepCount)) {
     terminerBtn.classList.add('d-block');
     nextBtn.classList.remove('d-block');
     nextBtn.classList.add('d-none');
-
   }
+}
+progress((100 / stepCount) * current_step);
+})
+
+prevBtn.addEventListener('click',() =>{
+  if (current_step > 0) {
+    current_step--;
+let previous_step = current_step + 1;
+stepform[current_step].classList.remove('d-none');
+stepform[current_step].classList.add('d-block')
+stepform[previous_step].classList.remove('d-block');
+stepform[previous_step].classList.add('d-none');
+if (current_step < stepCount) {
+  terminerBtn.classList.remove('d-block');
+  terminerBtn.classList.add('d-none');
+  nextBtn.classList.remove('d-none');
+  nextBtn.classList.add('d-block');
+  prevBtn.classList.remove('d-none');
+  prevBtn.classList.add('d-block');
 
 }
-
-
-
+  }
+  if (current_step == 0) {
+    prevBtn.classList.remove('d-block');
+    prevBtn.classList.add('d-none');
+  }
+progress((100 / stepCount) * current_step);
 })
