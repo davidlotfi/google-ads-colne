bf7dd8517db4226e871f610669b34707ee0c0340
davidlotfi <boukernineabdellatif@gmail.com>
2023-01-04 12:52:30 +0100

    form step

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let stepform =document.getElementsByClassName('form_etap');
+let nextBtn = document.getElementById('next-btn');
 let prevBtn = document.getElementById('prev');
+let terminer = document.getElementById('terminer-btn');
 
-  prevBtn.classList.add('d-none');
+let current_step = 0;
+let stepCount = 3;
+
+stepform[current_step].classList.add('d-block');
+prevBtn.classList.add('d-none');
