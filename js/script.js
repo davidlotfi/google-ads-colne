362a16550ffb9be33ba4e846e50d38d126528369
davidlotfi <boukernineabdellatif@gmail.com>
2023-01-11 13:11:25 +0100

    add js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let stepform =document.getElementsByClassName('form_etap');
 let nextBtn = document.getElementById('next-btn');
 let prevBtn = document.getElementById('prev');
 let terminerBtn = document.getElementById('terminer-btn');
+let succcessDiv = document.getElementById('success');
+
 
 let count = stepform.length
 let current_step = 0;
@@ -36,7 +38,7 @@ if ((current_step > 0) && (current_step <= stepCount)) {
   }
 }
 progress((100 / stepCount) * current_step);
-})
+});
 
 prevBtn.addEventListener('click',() =>{
   if (current_step > 0) {
@@ -61,4 +63,17 @@ if (current_step < stepCount) {
     prevBtn.classList.add('d-none');
   }
 progress((100 / stepCount) * current_step);
-})
+});
+
+terminerBtn.addEventListener('click', ()=>{
+
+  stepform[stepCount].classList.remove('d-block');
+  stepform[stepCount].classList.add('d-none');
+  prevBtn.classList.remove('d-block');
+  prevBtn.classList.add('d-none');
+  terminerBtn.classList.remove('d-block');
+  terminerBtn.classList.add('d-none');
+  succcessDiv.classList.remove('d-none');
+  succcessDiv.classList.add('d-block');
+
+});
